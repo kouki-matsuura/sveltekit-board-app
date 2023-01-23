a6877cefb6e231dd642812ed7b8661f0bd6209a3
松浦　昂生 <kouki19991222@icloud.com>
2023-01-23 21:45:34 +0900

    ヘッダーの作成

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+</script>
+
+<svelte:head>
+    <title>SvelteKit Auth</title>
+</svelte:head>
+
+<nav>
+    {#if !$page.data.user}
+        <a href="/login">ログイン</a>
+        <a href="/register">新規登録</a>
+    {/if}
+
+    {#if $page.data.user}
+        <a href="/admin">Admin</a>
+
+        <form action="/logout?/logout" method="POST">
+            <button>Log out</button>
+        </form>
+    {/if}
+</nav>
+
+<main>
+    <slot />
+</main>
